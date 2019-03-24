b662b2c782efd2010f08b2803b2e965a62655508
Aleksandr Kovsharov <sanforg2@gmail.com>
2019-03-24 09:34:43 +0200

    Prepared query created

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -1,19 +1,17 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
     private Connection connection;
-    private String usernameAccess = "select * from ( " +
-            "select u.username,  acc_path, acc_type from users u join accesses USING(username) " +
-            "union" +
-            " select u.username, ac.acc_path, ac.acc_type from users u join groups g on g.group_name = u.group_name " +
-            " join groups_accesses a on a.groups_name = g.group_name " +
-            " join accesses ac on ac.acc_path = a.acc_path and ac.acc_type = a.acc_type " +
-            ") alias ";
+    private String usernameAccess = "select * from (" +
+            " select u.username,  acc_path,  is_dir from users u join accesses USING(username)" +
+            " union " +
+            " select u.username, ac.acc_path, ac.is_dir from users u join groups g USING(group_name)" +
+            " join groups_accesses a USING(group_name) " +
+            " join accesses ac USING(acc_path, is_dir) " +
+            ") alias " +
+            "WHERE username = ?";
 
     public Database(Connection connection) {
         this.connection = connection;
@@ -21,10 +19,9 @@ public class Database {
 
     public boolean hasAccess(String username, String path){
         try {
-            try (Statement statement = connection.createStatement()){
-                ResultSet result = statement.executeQuery(
-                        usernameAccess + "WHERE username =" + "'" + username + "'");
-
+            try (PreparedStatement preparedStatement = connection.prepareStatement(usernameAccess)){
+                preparedStatement.setString(1, username);
+                ResultSet result = preparedStatement.executeQuery();
                 while (result.next()){
                     System.out.print(result.getString(1) + "\t");
                     System.out.println(result.getString(2));
